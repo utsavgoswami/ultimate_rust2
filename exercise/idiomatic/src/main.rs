a82743c0a1258d73ca08dcc0b8f69904b844c47c
utsavgoswami <utsavg19@googlemail.com>
2023-11-25 15:47:08 -0500

    Solve Idiomatic Ex.

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -7,17 +7,29 @@
 // 3. Time to clean up! Run `cargo clippy`. Fix up all the warnings so `cargo clippy` is silent.
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
-
-const pi:f32=3.14159265358979323846;
-fn count_to_5()->i32{let mut foo =0;loop{if foo>pi as i32{if foo > 5{break;}}foo=foo+1;}return 5;}
-fn main() {
-println!("I can count to {}", count_to_5());
+const PI: f32 = std::f32::consts::PI;
+fn count_to_5() -> i32 {
+    let mut foo = 0;
+    loop {
+        if foo > PI as i32 {
+            if foo > 5 {
+                break;
+            }
+        }
+        foo = foo + 1;
+    }
+    return 5;
 }
+
+fn main() {
+    println!("I can count to {}", count_to_5());
+}
+
 #[cfg(test)]
 mod test {
-use super::*;
-#[test]
-fn test_counting() {
-assert_eq!(count_to_5() == 5, true);
-}
+    use super::*;
+    #[test]
+    fn test_counting() {
+        assert_eq!(count_to_5() == 5, true);
+    }
 }
