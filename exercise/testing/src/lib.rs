73693dcf631aee359e1d9f60239f7b268ab48444
utsavgoswami <utsavg19@googlemail.com>
2023-12-16 19:26:47 -0800

    Impl UTs for Testing Ex

--- exercise/testing/src/lib.rs
+++ exercise/testing/src/lib.rs
@@ -12,24 +12,51 @@ pub fn splish(a: i32, b: i32) -> i32 {
 }
 
 // 1. Use the `cfg` attribute to mark the `test` module below as a test module
-
+#[cfg(test)]
 mod test {
     // 2. Bring all the library items into scope with a `use` statement
     // Hint: It's okay to use `*` here.
+    use super::*;
 
     // 3. Write a test function that verifies the following condition using the `assert_eq!` or
     // `assert_ne!` macros
     // - sploosh(1, 2, 3) returns 4
+
+    #[test]
+    fn sploosh_calculate_right_answer() {
+        assert_eq!(sploosh(1, 2, 3), 4);
+    }
     // - sploosh(5, 6, 7) does not return 4
+    #[test]
+    fn sploosh_calculate_not_return_wrong_answer() {
+        assert_ne!(sploosh(5, 6, 7), 4);
+    }
     // - If you pass sploosh a negative number for the first argument, 99 is returned
+    #[test]
+    fn sploosh_calculate_with_neg_num() {
+        assert_eq!(sploosh(-1, 6, 7), 99);
+    }
     //
     // `cargo test` should run your tests and pass
     // Hint: Don't forget the `#[test]` attribute for your test function!
 
     // 4. Write a test function that verifies the following conditions using the `assert!` macro
     // - splish(100, 10) is negative
+    #[test]
+    fn splish_ret_neg() {
+        assert!(splish(100, 10) < 0);
+    }
+
     // - splish(40, 20) is positive
+    #[test]
+    fn splish_ret_pos() {
+        assert!(splish(40, 20) > 0);
+    }
     // - splish(9, 3) is 0
+    #[test]
+    fn splish_ret_zero() {
+        assert!(splish(9, 3) == 0);
+    }
 }
 
 // 5. Create a `tests/` directory and an integration test file `tests/more_tests.rs`
